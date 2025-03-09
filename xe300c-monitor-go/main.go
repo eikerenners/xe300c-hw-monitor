66fe71126eaa98e5ca0d461d8828637f120deada
Your Name <you@example.com>
2025-03-09 12:15:37 +0100

    feat: Add main.go for xe300c-monitor-go project

---
+++ xe300c-monitor-go/main.go
@@ -0,0 +1,7 @@
+package main
+
+// write a simple go application that makes an http call to localhost similar to the following command: "curl -k http://192.168.8.1/rpc -H 'glinet: 1' -d '{"jsonrpc":"2.0","id":1,"method":"call","params":["","system","get_status"]}'". The result is parsed into json and printed to the console. AI!
+
+func main() {
+
+}
