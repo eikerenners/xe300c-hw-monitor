c7d49549bc01e053a532c90d49ff5b0c5fe2026f
Your Name <you@example.com>
2025-03-09 11:36:29 +0100

    feat: Add main.rs file to the project

---
+++ main.rs
@@ -0,0 +1,7 @@
+// Write a simple rust function that makes http get calls to localhost, similar to this shell command "curl -k http://127.0.0.1/rpc -H 'glinet: 1' -d '{"jsonrpc":"2.0","id":1,"method":"call","params":["","model","get_cells_info"]}'". As a return value, expect a json formatted string which needs to be parsed into an object. AI! 
+fn main() {
+  // Statements here are executed when the compiled binary is called.
+
+  // Print text to the console.
+  println!("Hello World!");
+}
